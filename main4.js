5f88a544fafe11f5c5325180951be66a89df6377
MAHAM42 <161121868+MAHAM42@users.noreply.github.com>
2024-04-24 10:53:41 -0700

    4th-Assignment
    TypeScript 45-Assignment of Q4 with Node.js

---
+++ main4.js
@@ -0,0 +1,13 @@
+"use strict";
+//ASSIGNMNT NO 04:
+Object.defineProperty(exports, "__esModule", { value: true });
+/*
+Find a quote form a famous person you admire. print the quote and the name of its author.
+your output should look something like the following, including the quotation marks:
+Albert Einstein once said, "A person who never made a mistake never tried anything new."
+*/
+console.log("ASSIGNMENT NO 04");
+const myQuote = `Albert Einstein once said,"A person who never made a mistake never
+tried anything new."`;
+//backtick
+console.log(myQuote);
---
+++ main4.ts
@@ -0,0 +1,18 @@
+//ASSIGNMNT NO 04:
+
+import { Console } from "console";
+
+/*
+Find a quote form a famous person you admire. print the quote and the name of its author.
+your output should look something like the following, including the quotation marks:
+Albert Einstein once said, "A person who never made a mistake never tried anything new." 
+*/
+
+console.log("ASSIGNMENT NO 04");
+
+const myQuote: string = `Albert Einstein once said,"A person who never made a mistake never
+tried anything new."` 
+
+//backtick
+
+console.log(myQuote);
